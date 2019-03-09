bcbf42c8a88061639dca1bebc4e019b8811da980
segasi <segasi@gmail.com>
2019-03-09 07:32:06 -0600

    Generar data frame con TODAS las palabras pronuncidas por AMLO en cada discurso

--- 02_codigo/analisis_discursos_tidytext.R
+++ 02_codigo/analisis_discursos_tidytext.R
@@ -1,3 +1,7 @@
 ### Cargar y limpiar datos ----
 # source("02_codigo/cargar_limpiar_datos.R") # Descomentar esta línea de código y ejecutarla sólo si antes no ejecutaste el archivo cargar_limpiar_datos.R
 
+### Generar data frame con TODAS las palabras pronuncidas por AMLO en cada discurso ----
+bd_palabras_todas <- 
+  discursos_amlo %>% 
+  unnest_tokens(word, texto)
