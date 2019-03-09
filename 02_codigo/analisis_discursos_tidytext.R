8701d815dc8ee04b66f998a5293dd3c4b60caad4
segasi <segasi@gmail.com>
2019-03-09 07:31:50 -0600

    Cargar y limpiar datos

--- 02_codigo/analisis_discursos_tidytext.R
+++ 02_codigo/analisis_discursos_tidytext.R
@@ -1,2 +1,3 @@
 ### Cargar y limpiar datos ----
 # source("02_codigo/cargar_limpiar_datos.R") # Descomentar esta línea de código y ejecutarla sólo si antes no ejecutaste el archivo cargar_limpiar_datos.R
+
