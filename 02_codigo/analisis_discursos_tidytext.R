68fdaa405067edd59e7eb5d9188f127b0604fea1
segasi <segasi@gmail.com>
2019-03-09 07:32:34 -0600

    Contar el número total de palabras pronunciadas por AMLO por discurso, incluyendo stopwords

--- 02_codigo/analisis_discursos_tidytext.R
+++ 02_codigo/analisis_discursos_tidytext.R
@@ -5,3 +5,11 @@
 bd_palabras_todas <- 
   discursos_amlo %>% 
   unnest_tokens(word, texto)
+
+### Contar el número total de palabras pronunciadas por AMLO por discurso, incluyendo stopwords ----
+bd_palabras_todas %>% 
+  group_by(discurso_id) %>% 
+  summarise(num_palabras = n()) %>% 
+  ungroup() %>% 
+  mutate(total_palabras = sum(num_palabras)) %>% 
+  arrange(-num_palabras)
