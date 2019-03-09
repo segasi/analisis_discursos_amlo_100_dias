b2865748879719d31d44543e85646525cd4bf23c
segasi <segasi@gmail.com>
2019-03-09 07:30:50 -0600

    Iniciar análisis de los discursos de AMLO

---
+++ 02_codigo/analisis_discursos_tidytext.R
@@ -0,0 +1,2 @@
+### Cargar y limpiar datos ----
+# source("02_codigo/cargar_limpiar_datos.R") # Descomentar esta línea de código y ejecutarla sólo si antes no ejecutaste el archivo cargar_limpiar_datos.R
